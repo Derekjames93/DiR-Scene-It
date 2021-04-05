facb92c821d182fffc36db74865b59cf1731ce5c
Derek James <derekjames1093@gmail.com>
2021-04-05 15:52:40 -0500

    add redux

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore } from "redux";
+import { movieReducer } from "./reducer";
+
+
+const store = createStore(
+    movieReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
